72d59af3b368e1aa352a7fae04fbac22252642cc
Donghwan Lee <colus4@gmail.com>
2016-12-18 12:18:15 +0900

    메소드에 간단한 주석 추가

--- src/main/java/codewars/WhichAreIn.java
+++ src/main/java/codewars/WhichAreIn.java
@@ -11,6 +11,12 @@ import static java.util.stream.Collectors.toList;
  * Created by colus on 2016. 12. 18..
  */
 public class WhichAreIn {
+    /**
+     * for문을 이용한 해결 방법
+     * @param array1
+     * @param array2
+     * @return
+     */
     public static String[] inArray_for(String[] array1, String[] array2) {
         if ( array1.length == 0 || array2.length == 0 ) {
             return new String[]{};
@@ -27,6 +33,13 @@ public class WhichAreIn {
         return result.toArray(new String[]{});
     }
 
+    /**
+     * Java 8 Stream API를 이용한 해결 방법
+     * 내부 비교하는 부분을 별도 함수로 처리함.
+     * @param array1
+     * @param array2
+     * @return
+     */
     public static String[] inArray_Stream1(String[] array1, String[] array2) {
         if ( array1.length == 0 || array2.length == 0 ) {
             return new String[]{};
@@ -46,6 +59,13 @@ public class WhichAreIn {
         return false;
     }
 
+    /**
+     * Java 8 Stream API를 이용한 해결 방법
+     * 내부 처리도 Stream API를 이용함.
+     * @param array1
+     * @param array2
+     * @return
+     */
     public static String[] inArray_Stream2(String[] array1, String[] array2) {
         if ( array1.length == 0 || array2.length == 0 ) {
             return new String[]{};
@@ -58,7 +78,12 @@ public class WhichAreIn {
                 .collect(toList()).toArray(new String[]{});
     }
 
-
+    /**
+     * Arrays의 stream을 이용한 해결 방법
+     * @param array1
+     * @param array2
+     * @return
+     */
     public static String[] inArray_ArraysStream(String[] array1, String[] array2) {
         if ( array1.length == 0 || array2.length == 0 ) {
             return new String[]{};
